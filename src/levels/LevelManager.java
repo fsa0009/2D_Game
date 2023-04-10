c70c6010309157424f12e78157bb5ecc954ff994
Faham Al Aamri <f.aamri2000@gmail.com>
2023-04-09 22:45:31 -0400

    Map Layer over background

--- src/levels/LevelManager.java
+++ src/levels/LevelManager.java
@@ -10,16 +10,20 @@ public class LevelManager {
 
 	private Game game ; 
 	private BufferedImage levelSprite ; 
+	private BufferedImage background ; 
 	
 	public LevelManager(Game game) {
 		this.game = game ; 
-		
 		levelSprite = LoadSave.getSpriteAtlas(LoadSave.LEVEL_ATLAS) ; 
+		background = LoadSave.getSpriteAtlas(LoadSave.LEVEL_BACKGROUND) ; 
 	}
 	
 	
+	
 	public void draw(Graphics g) {
+		g.drawImage(background, 0, 0,1248 ,672,  null) ; 
 		g.drawImage(levelSprite, 0, 0,1248 ,672,  null) ; 
+
 	}
 	
 	public void update() {
--- src/utilz/LoadSave.java
+++ src/utilz/LoadSave.java
@@ -10,6 +10,8 @@ public class LoadSave {
 	
 	public static final String LEVEL_ATLAS = "/Assets/map.png";
 
+	public static final String LEVEL_BACKGROUND = "/Assets/Background.jpg";
+	
 	public static BufferedImage getSpriteAtlas(String fileName) {
 		BufferedImage img = null; 
 		InputStream is = LoadSave.class.getResourceAsStream(fileName); 
@@ -26,6 +28,24 @@ public class LoadSave {
 		}
 		return img; 
 	}
+	public static BufferedImage getBackground(String fileName) {
+		BufferedImage img = null; 
+		InputStream is = LoadSave.class.getResourceAsStream(fileName); 
+		try {
+			img = ImageIO.read(is); 
+		}catch (IOException e ) {
+			e.printStackTrace();
+		}finally {
+			try {
+				is.close(); 
+			}catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		return img; 
+	}
+	
+	
 	
 
 }
